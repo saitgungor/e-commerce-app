f3d42e5dae2726cd7135f4f176490b2ec2e38409
saitgungor <osmansaitgungor@gmail.com>
2022-10-21 09:17:32 +0300

    some bugs fixed for netlify

--- components/NotificationBar.js
+++ components/NotificationBar.js
@@ -3,7 +3,7 @@ const NotificationBar = () => {
     <div className="bg-black text-white w-full h-6 overflow-hidden">
       <div className="relative text-center">
         <div className="text_slider absolute text-center w-full">
-          14:00'a kadar verilen siparişleriniz aynı gün kargoda.
+          {"14:00'a kadar verilen siparişleriniz aynı gün kargoda."}
         </div>
         <div className="text_slider absolute text-center w-full">
           60 gün içerisinde koşulsuz iade hakkı!
