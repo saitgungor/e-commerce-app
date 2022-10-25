81db16de8862ac2c044957a39595d8aa3b2d4f83
saitgungor <osmansaitgungor@gmail.com>
2022-10-25 14:03:04 +0300

    started the adding new collection section

---
+++ components/NewCollection.js
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+const NewCollection = () => {
+  return (
+    <div className="h-[480px] mt-10 w-[80%] m-auto flex">
+      <div className="w-1/2 bg-[url('/deneme-6.jpg')] bg-no-repeat bg-contain"></div>
+      <div className="w-1/2 bg-red-500 ">asdas</div>
+    </div>
+  );
+};
+export default NewCollection;
