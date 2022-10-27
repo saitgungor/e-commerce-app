123b65ca5abf22159a574b83eb3e92c6ea2ee008
saitgungor <osmansaitgungor@gmail.com>
2022-10-27 14:55:03 +0300

    footer added

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import Slider from '../components/Slider';
 import NewCollection from '../components/NewCollection';
 import BestSellers from '../components/BestSellers';
 import Collection from '../components/Collections';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
@@ -20,6 +21,7 @@ export default function Home() {
       <NewCollection />
       <BestSellers />
       <Collection />
+      <Footer />
     </Fragment>
   );
 }
